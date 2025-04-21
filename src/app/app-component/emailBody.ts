4717221b2c211beaf5047a3381a27aa73c049d4e
Luna <23488462+LunaMoraes@users.noreply.github.com>
2025-04-21 12:55:41 -0300

    emailbody construido

--- src/app/app-component/emailBody.ts
+++ src/app/app-component/emailBody.ts
@@ -1,3 +1,44 @@
 export const emailBody = [
-    "Venho por meio deste falar o"
+    `Vossas excelências, senadores e deputados. 
+Drs representantes de seus respectivos conselhos.
+
+
+Solicitamos respeitosamente ações e intervenções acerca da nota técnica 2.427/2025 do CFM.
+
+
+A comunidade trans brasileira, historicamente marginalizada e violentada pelo Estado e pela sociedade, NÃO ACEITARÁ MAIS UM ATAQUE AOS SEUS DIREITOS FUNDAMENTAIS. A recente resolução do Conselho Federal de Medicina (CFM), que restringe o acesso de adolescentes trans a tratamentos médicos adequados e baseados em evidências científicas, é mais um capítulo de um projeto sistemático de desmonte das políticas públicas conquistadas após décadas de luta.
+
+
+O Posicionamento técnico da sociedade brasileira de medicina de família nos mostra a real intenção da nota técnica 2427/2025 CFM, que não é pautada em evidências científicas robustas e globalmente aceitas, servindo para mostrar o papel ideológico que o conselho assumiu tentando desmontar um programa de cuidado que graças à luta da comunidade LGBTQIAP+ em especial trans e travestis tem conseguido lentos mas importantes avanços. Esta nota é um enorme retrocesso em tudo construído pela comunidade e terá impacto direto na saúde mental e física da população Trans
+
+
+Entre as mudanças mais preocupantes, está a proibição do uso de bloqueadores hormonais para crianças e adolescentes com variabilidade de gênero ou incongruência ao gênero designado no nascimento — uma intervenção que, quando bem indicada e acompanhada por equipes multiprofissionais, é considerada segura e eficaz para reduzir sofrimento psíquico e prevenir consequências irreversíveis da puberdade indesejada. Resolução também eleva a idade mínima para cirurgias de afirmação de gênero com efeitos esterilizantes para 21 anos, negando o direito de autodeterminação de pessoas trans adultas. 
+
+
+Além disso, há agora exigência de acompanhamento em múltiplas especialidades para o tratamento hormonal, o que completamente desconsidera a autonomia das equipes de saúde básica além de incorrer em uma barreira efetiva de demandas de especialidades de urologia, ginecologia, psiquiatria e endocrinologia completamente incompatíveis com a demanda atual dos sistemas de saúde, sendo efetivamente um impeditivo para o acesso de pessoas trans adultas aos ambulatórios públicos devido às enormes filas enfrentadas, além de prejudicar gravemente a população geral, que verá um enorme aumento nas filas das especialidades médicas
+
+
+
+
+
+
+Por isso, Exigimos:
+
+
+1 A revogação imediata da resolução do CFM, que proíbe o uso de bloqueadores hormonais em adolescentes trans e restringe cirurgias de corrigir linha
+e afirmação de gênero apenas para maiores de 21 anos;
+2 A garantia de acesso irrestrito à saúde trans no SUS, incluindo o uso de bloqueadores hormonais até os 18 anos, e o início do processo de hormonização a partir dos 16 anos com consentimento dos responsáveis legais — ou de forma autônoma a partir dos 18 anos. Reivindicamos também o acompanhamento multiprofissional, com base em diretrizes científicas e nos princípios da autonomia progressiva previstos no ECA.
+3 A retomada urgente da Política Nacional de Saúde Integral LGBT, com orçamento e estrutura adequados;
+4 A participação efetiva de pessoas trans e travestis na elaboração de políticas públicas que afetam essa comunidade.
+
+
+Endossam este pedido:
+Coletivo Chama
+IBRAT-SP
+Sport Clube T Mosqueteiros
+Ambulatório Transcender - Campinas/SP
+ABRAI (Associação Brasileira Intersexo)
+Mães pela Diversidade 
+Associação Famílias e Resistência - AFeR
+`
 ]
